838b46885c101e6c0b8b0fe7abcd92665699196c
Kevin <niavo.kevin9@gmail.com>
2025-07-28 14:59:30 +0300

    ajout du bouton qu remonte

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,10 @@ import Contact from "./Layouts/Pages/Contact";
 import Footer from "./Layouts/Components/Footer";
 import { Parallax } from 'react-scroll-parallax';
 import carte from './Layouts/Assets/Carte nadia.png';
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
-export default function App()
-{
-  // Etat
+export default function App() {
+  // État du dialogue de téléchargement
   const [showDialog, setShowDialog] = useState(false);
 
   const handleDownload = () => {
@@ -26,63 +25,103 @@ export default function App()
     link.click();
     setShowDialog(false);
   };
-  // Affichage
-  return(
+
+  // État pour gérer la visibilité du bouton de scroll vers le haut
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      setIsVisible(window.scrollY > 100);
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
     <div className="">
       <NavBar />
       <Accueil />
-      <div className="w-full h-[70vh] bg-white p-2 bg-fixed inset-0 flex justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500" style={{ backgroundImage : `url(${Logo})` }}></div>
-      <Réalisation />
-      <div className="w-full h-[40vh] lg:h-[70vh] bg-white p-2 bg-fixed overflow-hidden inset-0 flex lg:flex-row-reverse justify-center lg:justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500 fond4">
-        <Parallax speed={-25}  rotate={[-10 , 10]} className="text-white text-4xl lg:text-8xl font-extrabold tracking-widest lg:mr-20">COMPETENCE</Parallax>
-        <Parallax speed={-25} rotate={[10 , -10]} className="text-white text-3xl lg:text-8xl font-extrabold tracking-widest ml-20"><FaBrain /></Parallax>
-      </div>
-      <Compétences />
-      <div className="w-full h-[40vh] lg:h-[70vh] bg-white p-2 bg-fixed overflow-hidden inset-0 flex lg:flex-row justify-center lg:justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500 fond1">
-        <Parallax speed={-25} rotate={[10 , -10]} className="text-white text-4xl lg:text-8xl font-extrabold tracking-widest lg:ml-20">EXPERIENCE</Parallax>
-        <Parallax speed={-25} rotate={[10 , -10]} className="text-white text-3xl lg:text-8xl font-extrabold tracking-widest ml-20 lg:mr-20 lg:ml-0"><AiFillCheckCircle /></Parallax>
-      </div>
-      <Expérience />
-      <div className="w-full h-[40vh] lg:h-[70vh] bg-white p-2 bg-fixed overflow-hidden inset-0 flex lg:flex-row justify-center lg:justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500 fond3">
-        <Parallax speed={-25} rotate={[10 , -10]} className="text-white text-4xl lg:text-8xl font-extrabold tracking-widest lg:ml-20">Contact</Parallax>
-        <Parallax speed={-25} rotate={[10 , -10]} className="text-white text-3xl lg:text-8xl font-extrabold tracking-widest ml-20 lg:mr-20 lg:ml-0"><MdContacts /></Parallax>
-      </div>
-      <div className="container w-full h-[20vh] lg:h-[25vh] my-20 relative justify-center items-center flex">
-        <img src={carte} alt="Carte de visite" className='absolute w-[95%] lg:w-[50%] cursor-pointer h-full border-2 border-cyan-800' onClick={() => setShowDialog(true)} />
 
-        {/* Donwload */}
-         {showDialog && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center fond3 bg-opacity-60">
-          <div className="w-full max-w-sm p-6 space-y-4 bg-white shadow-xl rounded-xl">
-            <h2 className="text-lg font-semibold text-gray-800">Stand'art | Carte de visite ?</h2>
-            <p className="text-sm text-gray-600">Contactez nous le plus rapidement .</p>
-            <div className="flex justify-end space-x-4">
-              <button
-                onClick={() => setShowDialog(false)}
-                className="px-4 py-2 text-sm bg-gray-200 rounded hover:bg-gray-300"
-              >
-                Annuler
-              </button>
-              <button
-                onClick={handleDownload}
-                className="px-4 py-2 text-sm text-white bg-blue-600 rounded hover:bg-blue-700"
-              >
-                Télécharger
-              </button>
+      <div
+        className="w-full h-[70vh] bg-white p-2 bg-fixed inset-0 flex justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500"
+        style={{ backgroundImage: `url(${Logo})` }}
+      ></div>
+
+      <Réalisation />
+
+      <div className="w-full h-[40vh] lg:h-[70vh] bg-white p-2 bg-fixed overflow-hidden inset-0 flex lg:flex-row-reverse justify-center lg:justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500 fond4">
+        <Parallax speed={-25} rotate={[-10, 10]} className="text-white text-4xl lg:text-8xl font-extrabold tracking-widest lg:mr-20">COMPETENCE</Parallax>
+        <Parallax speed={-25} rotate={[10, -10]} className="text-white text-3xl lg:text-8xl font-extrabold tracking-widest ml-20"><FaBrain /></Parallax>
+      </div>
+
+      <Compétences />
+
+      <div className="w-full h-[40vh] lg:h-[70vh] bg-white p-2 bg-fixed overflow-hidden inset-0 flex lg:flex-row justify-center lg:justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500 fond1">
+        <Parallax speed={-25} rotate={[10, -10]} className="text-white text-4xl lg:text-8xl font-extrabold tracking-widest lg:ml-20">EXPERIENCE</Parallax>
+        <Parallax speed={-25} rotate={[10, -10]} className="text-white text-3xl lg:text-8xl font-extrabold tracking-widest ml-20 lg:mr-20 lg:ml-0"><AiFillCheckCircle /></Parallax>
+      </div>
+
+      <Expérience />
+
+      <div className="w-full h-[40vh] lg:h-[70vh] bg-white p-2 bg-fixed overflow-hidden inset-0 flex lg:flex-row justify-center lg:justify-between bg-contain bg-no-repeat bg-center items-center text-blue-500 fond3">
+        <Parallax speed={-25} rotate={[10, -10]} className="text-white text-4xl lg:text-8xl font-extrabold tracking-widest lg:ml-20">Contact</Parallax>
+        <Parallax speed={-25} rotate={[10, -10]} className="text-white text-3xl lg:text-8xl font-extrabold tracking-widest ml-20 lg:mr-20 lg:ml-0"><MdContacts /></Parallax>
+      </div>
+
+      <div className="container w-full h-[20vh] lg:h-[25vh] my-20 relative justify-center items-center flex">
+        <img
+          src={carte}
+          alt="Carte de visite"
+          className='absolute w-[95%] lg:w-[50%] cursor-pointer h-full border-2 border-cyan-800'
+          onClick={() => setShowDialog(true)}
+        />
+
+        {/* Dialogue téléchargement */}
+        {showDialog && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center fond3 bg-opacity-60">
+            <div className="w-full max-w-sm p-6 space-y-4 bg-white shadow-xl rounded-xl">
+              <h2 className="text-lg font-semibold text-gray-800">Stand'art | Carte de visite ?</h2>
+              <p className="text-sm text-gray-600">Contactez nous le plus rapidement.</p>
+              <div className="flex justify-end space-x-4">
+                <button
+                  onClick={() => setShowDialog(false)}
+                  className="px-4 py-2 text-sm bg-gray-200 rounded hover:bg-gray-300"
+                >
+                  Annuler
+                </button>
+                <button
+                  onClick={handleDownload}
+                  className="px-4 py-2 text-sm text-white bg-blue-600 rounded hover:bg-blue-700"
+                >
+                  Télécharger
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-
+        )}
       </div>
+
       <Contact />
       <div className="w-full h-20 bg-white" />
       <Footer />
 
-      <div className="fixed bottom-5 right-5 z-40 w-[4%] h-[8vh] fond3 flex items-center justify-center border rounded-full">
-        <AiOutlineArrowUp className="text-5xl text-white"/>
-      </div>
-
+      {/* Bouton Scroll to Top */}
+      {isVisible && (
+        <div
+          onClick={scrollToTop}
+          className="fixed bottom-5 right-5 z-40 w-[4%] h-[8vh] fond3 flex items-center justify-center border rounded-full cursor-pointer transition-opacity duration-500"
+        >
+          <AiOutlineArrowUp className="text-5xl text-white" />
+        </div>
+      )}
     </div>
-  )
+  );
 }
